5d3eea712d54bbed17a0905bd587d6c76be420d9
mkslofstra <mkslofstra@bin259.bin>
2015-02-12 15:04:59 +0100

    package bodyFeatures added

---
+++ src/java/nl/bioinf/mkslofstra/DiseaseFinder/bodyFeatures/PrenatalManifestations.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package nl.bioinf.mkslofstra.DiseaseFinder.bodyFeatures;
+
+/**
+ *
+ * @author mkslofstra
+ */
+public class PrenatalManifestations extends Body{
+    
+}
