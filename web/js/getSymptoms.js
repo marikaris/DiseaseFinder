6fb1edfe00f9acff665202ae8fc013e28b6fab2f
mkslofstra <mkslofstra@bin255.bin>
2015-04-14 16:11:20 +0200

    working on selecting parent nodes

--- web/js/getSymptoms.js
+++ web/js/getSymptoms.js
@@ -4,13 +4,23 @@ function initialize() {
 
     $("#ontology-tree").on('changed.jstree', function(e, data) {
         var i, j, r = [];
+        var parents = [];
         for (i = 0, j = data.selected.length; i < j; i++) {
-            r.push(data.instance.get_node(data.selected[i]).text);
+            var selected = data.instance.get_node(data.selected[i]);
+            r.push(selected.text);
+//            console.log(selected.parents("li"));
+            
         }
         $('#event_result').html('Selected: ' + r.join(', '));
         $("#search-button").click(function() {
-            console.log("***" + r);
+//            console.log("***" + r);
         });
+//        console.log($("input:checked").id);
+        var parents = [];
+        selected.parents("li").each(function() {
+            parents.push({id: $(this).attr("id"), description: $(this).children("a").text()});
+        });
+        console.log(parents);
     })
             // create the instance
             ;
