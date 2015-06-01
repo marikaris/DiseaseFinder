fc39ccc0ff40b04b5b022720f5812c6e26a6568f
mkslofstra <mkslofstra@bin259.bin>
2015-06-01 11:28:41 +0200

    selected symptoms work better now
    version works

--- web/js/resizePics.js
+++ web/js/resizePics.js
@@ -1,6 +1,8 @@
+//by mkslofstra
 $(document).ready(initialize);
 function initialize() {
     $(".examplePic").click(function() {
+        //toggle the class so the css will make the pic bigger
         $(this).toggleClass("bigger");
     });
     $('[data-toggle="tooltip"]').tooltip();
