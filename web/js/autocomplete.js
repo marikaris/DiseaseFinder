8cb5bb9a38585b073a7228f5f5c6664e6225d579
aroeters <aroeters@bin258.bin>
2015-06-25 12:00:01 +0200

    autocomplete done!!! woohooo

--- web/js/autocomplete.js
+++ web/js/autocomplete.js
@@ -3,6 +3,9 @@ $(document).ready(initialize);
 
 function initialize() {
     var url = "SearchBarAutocomplete.do";
+    $("#search-symptom").on("click", function() {
+        $(this).val("");
+    });
     $("#search-symptom").autocomplete({
         width: 150,
         max: 10,
@@ -11,33 +14,39 @@ function initialize() {
         cacheLength: 1,
         scroll: true,
         highlight: false,
-        source: function (request, response) {
+        source: function(request, response) {
             $.ajax({
                 url: url,
                 dataType: "json",
                 data: request,
-                success: function (data) {
+                success: function(data) {
                     var items = data;
                     response(items);
                 }
             });
         },
-        select: function (e, ui) {
+        select: function(e, ui) {
             $.ajax({
                 url: "SearchTree.do",
                 dataType: "text",
                 data: {"autoCompleteResult": ui.item.value},
-                success: function (data) {
+                success: function(data) {
                     data = data.replace(/\[|\]/g, "");
                     var newData = data.replace(/\"|\n/g, "").split(",").reverse();
-                    alert(newData);
-                    while (newData.length !== 0) {
-                    $("#ontology-tree").jstree("open_node", newData[0]);
-                    if (newData.length === 1) {
-                        $.jstree._reference("#ontology-tree").check_node('#'+newData[0]);
-                    }
-                    newData.splice(0,1);
-                    }
+                    var count = newData.length;
+                    window.setInterval(function() {
+                        $("#ontology-tree").jstree("open_node", newData[0]);
+                        if (count === 1) {
+                            // to highligt the symptom that is searched for
+                            $("#ontology-tree").jstree(true).get_node(newData[0]).li_attr.class = "jstree-search";
+                            window.clearInterval();
+                        }
+                        newData.splice(0, 1);
+                        count--;
+                    }, 500);
+                    $("#search-symptom").on("click", function() {
+                        $("*").removeClass("jstree-search");
+                    });
 
                 }
             });
